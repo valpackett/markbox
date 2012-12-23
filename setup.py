be4fd30a63c66246e3b1e72e5970433769edbbaa
myfreeweb <floatboth@me.com>
2012-12-23 17:30:38 +0400

    0.1.9

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if sys.version < "2.5":
 
 setup(
     name="markbox",
-    version="0.1.8",
+    version="0.1.9",
     description="A blogging engine for Dropbox based on Markdown",
     # long_description="""""",
     license="Apache License 2.0",
